d33fa84d99de55daa3ab88ed3bef9c7c4fdae25d
AL MAHMUD SURUJ <mahmudsuruj14@gmail.com>
2021-09-11 21:29:58 +0800

    third commit

---
+++ quiz.js
@@ -0,0 +1,18 @@
+const name = 'Hero';
+const age = 34;
+const person1 = {name, age};
+//  console.log(person);
+
+const adventurer = { name: 'Alice',cat: {name: 'Lucy'} };
+
+const ratName = adventurer?.rat?.name;
+// console.log(ratName);
+
+let person =null;
+
+console.log(person ? person : "person is null");
+
+const obj = {a:1};
+
+console.log(Object.keys(obj).length===0);
+
